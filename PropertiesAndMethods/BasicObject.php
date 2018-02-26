4649a3d40a7eb18f3f6eadfd26d999db98208e9a
Tina Nguyen <nguyentina2005@yahoo.com>
2018-02-25 19:01:09 -0500

    First commit

---
+++ PropertiesAndMethods/BasicObject.php
@@ -0,0 +1,34 @@
+<?php
+
+//creating a class
+class Bicycle{
+
+//class properties
+    var $brand;
+    var $model;
+    var $year;
+    var $description = 'Used bicycle';
+    var $weight_kg = 0.0;
+
+//class methods
+    function name(){
+        return $this->brand . " " . $this->model . "(" . $this->year . ")";
+    }
+    
+    function weight_lbs(){
+        return floatval($this->weight_kg) * 2.2046446218;
+    }
+
+    function setWeight_lbs($value){
+        $this->weight_kg = floatval($value) / 2.2046446218; 
+    }
+}
+
+$hybrid = new Bicycle;
+$hybrid->brand = 'Schwinn';
+$hybrid->model = 'SuperSport';
+$hybrid->year = '2017';
+$hybrid->weight_kg= 8.0;
+
+echo $hybrid->name();
+echo $hybrid->weight_lbs();
